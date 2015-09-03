cf6a6e841978115ac8332c722fc45e2048ee0daa
Tristan Strange <tristan.strange@gmail.com>
2015-09-03 15:23:25 +0100

    Updated web page and version info.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject midi-data "0.0.1-SNAPSHOT"
+(defproject midi-data "0.1.0"
   :description "Handles parsing of MIDI data tuples to maps containing friendly
                representations of the events they represent."
+  :url "https://github.com/triss/midi-data/"
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
